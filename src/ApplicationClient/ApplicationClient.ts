f5adbb6b87bb33a55b8942999cebc894baeb9b43
BothimTV <development@bothimtv.com>
2024-06-12 17:13:17 +0200

    WIP: Rebase application classes

---
+++ src/ApplicationClient/ApplicationClient.ts
@@ -0,0 +1,215 @@
+import { AxiosRequestConfig } from "axios";
+import { CreateUser, Location, LocationList, Node, NodeList, Server, ServerList, User } from "../../types/ApplicationApiResponse";
+import { LocationCreateProperties } from "../../types/RequestBodies";
+import { ClientOptions } from "../../types/Util";
+import { PanelNodeBuilder } from "../builder/PanelNodeBuilder";
+import { PanelUserBuilder } from "../builder/PanelUserBuilder";
+import { ServerBuilder } from "../builder/ServerBuilder";
+import { ApiRequestHandler } from "../functions/axois";
+import { RawPanelUserList } from "../types/PanelUser";
+import { ApplicationServer } from "./ApplicationServer";
+import { PanelLocation } from "./PanelLocation";
+import { PanelNode } from "./PanelNode";
+import { PanelUser } from "./PanelUser";
+
+export class ApplicationClient {
+  protected apikey: string;
+  public panel: string;
+  public api = async function api(config: AxiosRequestConfig, errorSet?: Array<{ code: number; message: string; }>, ignoredErrors?: Array<string>): Promise<any> { };
+  constructor(options: ClientOptions) {
+    this.apikey = "Bearer " + options.apikey;
+    this.panel = options.panel;
+    this.api = new ApiRequestHandler(this.apikey).axiosRequest
+    try {
+      new URL(this.panel);
+    } catch (error) {
+      throw new Error("Invalid panel url");
+    }
+  }
+
+  /**
+   *
+   *  User management
+   *
+   */
+  /**
+   * Get all users associated to the panel
+   * @param filter Filter users by email, uuid, username and/or externalId
+   * @param sortBy Sort users by id oder uuid
+   * @param reverseSort Reverse the sort @requires sortBy
+   */
+  public async getUsers(filter?: { email?: string; uuid?: string; username?: string; externalId?: string; }, sortBy?: "id" | "uuid", reverseSort?: boolean): Promise<Array<PanelUser>> {
+    var endpoint = new URL(this.panel + "/api/application/users?include=servers"); // FIXME: Include servers in data
+    if (filter?.email) endpoint.searchParams.append("filter[email]", filter.email);
+    if (filter?.username) endpoint.searchParams.append("filter[username]", filter.username);
+    if (filter?.uuid) endpoint.searchParams.append("filter[uuid]", filter.uuid);
+    if (filter?.externalId) endpoint.searchParams.append("filter[external_id]", filter.externalId);
+    if (sortBy) endpoint.searchParams.append("sort", `${reverseSort ? "-" : ""}${sortBy}`);
+    const users = await this.api({ url: endpoint.href, }) as RawPanelUserList
+    return users.data.map(user => new PanelUser(this, user));
+  }
+
+  /**
+   * Get a user via their userId
+   * @param userId The id of a user
+   */
+  public async getUser(userId: number): Promise<PanelUser> {
+    const endpoint = new URL(this.panel + "/api/application/users/" + userId + "?include=servers"); // FIXME: Include servers in data
+    return new PanelUser(this, await this.api({ url: endpoint.href }) as User);
+  }
+
+  /**
+  * Get a user via their external id
+  * @param externalId The external id of a user
+  */
+  public async getExternalUser(externalId: string): Promise<PanelUser> {
+    const endpoint = new URL(this.panel + "/api/application/users/external/" + externalId + "?include=servers");  // FIXME: Include servers in data
+    return new PanelUser(this, await this.api({ url: endpoint.href }) as User)
+  }
+
+  /**
+   * Create a user
+   * @param userProperties Construct a user via new PanelUserBuilder()
+   */
+  public async createUser(userProperties: PanelUserBuilder): Promise<PanelUser> {
+    const endpoint = new URL(this.panel + "/api/application/users");
+    return new PanelUser(this, await this.api({ url: endpoint.href, method: "POST", data: userProperties }, [{ code: 422, message: "There is already a user with this email and/or username" }]) as CreateUser);
+  }
+
+  /**
+   * Get all nodes of a panel
+   */
+  public async getNodes(): Promise<Array<PanelNode>> {
+    const endpoint = new URL(this.panel + "/api/application/nodes?include=allocations,location,servers");  // FIXME: Include include data
+    const data = await this.api({ url: endpoint.href }) as NodeList
+    const res: Array<PanelNode> = [];
+    for (const node of data.data) {
+      res.push(new PanelNode(this, node));
+    }
+    return res;
+  }
+
+  /**
+   * Get a specific Node by nodeId
+   * @param nodeId The id of the specific node
+   */
+  public async getNode(nodeId: number): Promise<PanelNode> {
+    const endpoint = new URL(this.panel + "/api/application/nodes/" + nodeId + "?include=allocations,location,servers"); // FIXME: Include include data
+    return new PanelNode(this, await this.api({ url: endpoint.href }) as Node)
+  }
+
+  /**
+  * Create a node
+  * @param nodeProperties The properties for the new node
+  */
+  public async createNode(nodeProperties: PanelNodeBuilder): Promise<PanelNode> {
+    const endpoint = new URL(this.panel + "/api/application/nodes");
+    return new PanelNode(this, await this.api({ url: endpoint.href, method: "POST", data: nodeProperties }) as Node)
+  }
+
+  /**
+  * Get the locations of this panel
+  */
+  public async getLocations(): Promise<Array<PanelLocation>> {
+    const endpoint = new URL(this.panel + "/api/application/locations?include=nodes,servers"); // FIXME: Include include data
+    const data = await this.api({ url: endpoint.href }) as LocationList
+    const res: Array<PanelLocation> = [];
+    for (const location of data.data) {
+      res.push(new PanelLocation(this, location));
+    }
+    return res;
+  }
+
+  /**
+  * Get a location of this panel
+  */
+  public async getLocation(locationId: number): Promise<PanelLocation> {
+    const endpoint = new URL(this.panel + "/api/application/locations/" + locationId + "?include=nodes,servers");
+    return new PanelLocation(this, await this.api({ url: endpoint.href }) as Location)
+  }
+
+  /**
+  * Create a location for this panel
+  */
+  public async createLocation(locationProperties: LocationCreateProperties): Promise<PanelLocation> {
+    const endpoint = new URL(this.panel + "/api/application/locations");
+    return new PanelLocation(this, await this.api({ url: endpoint.href, method: "POST", data: locationProperties }) as Location)
+  }
+
+  /**
+  * Get servers for this panel
+  */
+  public async getServers(): Promise<Array<ApplicationServer>> {
+    const endpoint = new URL(this.panel + "/api/application/servers");
+    const data = await this.api({ url: endpoint.href }) as ServerList
+    const res: Array<ApplicationServer> = [];
+    for (const server of data.data) {
+      res.push(new ApplicationServer(this, server));
+    }
+    return res;
+  }
+
+  /**
+  * Get a server for this panel
+  * @param serverId The target server Id
+  */
+  public async getServer(serverId: number): Promise<ApplicationServer> {
+    const endpoint = new URL(this.panel + "/api/application/servers/" + serverId + "?include=allocations,user,subusers,pack,nest,egg,variables,location,node,databases"); // FIXME: Include include data
+    return new ApplicationServer(this, await this.api({ url: endpoint.href }) as Server)
+  }
+
+  /**
+  * Get a server by their external id
+  * @prop externalId The target servers external id
+  */
+  public async getExternalServer(externalId: string): Promise<ApplicationServer> {
+    const endpoint = new URL(this.panel + "/api/application/servers/external/" + externalId + "?include=allocations,user,subusers,pack,nest,egg,variables,location,node,databases"); // FIXME: Include include data
+    return new ApplicationServer(this, await this.api({ url: endpoint.href }) as Server)
+  }
+
+  /**
+  * Create a server
+  */
+  public async createServer(serverProperties: ServerBuilder): Promise<ApplicationServer> {
+    const endpoint = new URL(this.panel + "/api/application/servers");
+    return new ApplicationServer(this, await this.api({ url: endpoint.href, method: "POST", data: serverProperties }) as Server)
+  }
+
+  /**
+   * Get the nests of this panel
+   */
+  public async getNests(): Promise<Array<any>> {
+    const endpoint = new URL(this.panel + "/api/application/nests?include=eggs,servers"); // FIXME: Include include data
+    throw new Error("Not implemented yet");
+  }
+
+  /**
+   * Get a nest by id of this panel
+   * @param nestId The id of the nest you want to get
+   */
+  public async getNest(nestId: number): Promise<any> {
+    const endpoint = new URL(this.panel + "/api/application/nests/" + nestId + "?include=eggs,servers"); // FIXME: Include include data
+    throw new Error("Not implemented yet");
+  }
+
+  /**
+   * Get the eggs of this panel
+   * @param nestId The if of the nest you want to get the eggs from 
+   */
+  public async getEggs(nestId: number): Promise<Array<any>> {
+    const endpoint = new URL(this.panel + "/api/application/nests/" + nestId + "/eggs?include=nest,servers,config,script,variables"); // FIXME: Include include data
+    throw new Error("Not implemented yet");
+  }
+
+  /**
+   * Get a specific egg of this panel
+   * @param nestId The if of the nest you want to get the egg from
+   * @param eggId The egg you want to get 
+   */
+  public async getEgg(nestId: number, eggId: number): Promise<any> {
+    const endpoint = new URL(this.panel + "/api/application/nests/" + nestId + "/eggs/" + eggId + "?include=nest,servers,config,script,variables"); // FIXME: Include include data
+    throw new Error("Not implemented yet");
+  }
+
+
+}
---
+++ src/ApplicationClient/PanelUser.ts
@@ -0,0 +1,213 @@
+import { PanelUserAttributes, RawPanelUser } from "../types/PanelUser";
+import { RawServer } from "../types/Server";
+import { ApplicationClient } from "./ApplicationClient";
+import { Server } from "./Server";
+
+var client: ApplicationClient;
+var relationships:
+  | {
+      servers?: {
+        object: "list";
+        data: Array<RawServer>;
+      };
+    }
+  | undefined;
+export class PanelUser implements PanelUserAttributes {
+  public readonly id: number;
+  public external_id?: string | null;
+  public readonly uuid: string;
+  public username: string;
+  public email: string;
+  public first_name: string;
+  public last_name: string;
+  public language: "en" | string;
+  public root_admin: boolean;
+  public readonly "2fa": boolean;
+  public readonly created_at: Date;
+  public updated_at: Date;
+  public servers?: Array<Server>;
+
+  constructor(applicationClient: ApplicationClient, userData: RawPanelUser) {
+    client = applicationClient;
+    this.id = userData.attributes.id;
+    this.external_id = userData.attributes.external_id;
+    this.uuid = userData.attributes.uuid;
+    this.username = userData.attributes.username;
+    this.email = userData.attributes.email;
+    this.first_name = userData.attributes.first_name;
+    this.last_name = userData.attributes.last_name;
+    this.language = userData.attributes.language;
+    this.root_admin = userData.attributes.root_admin; // NOTE: Not tested - might be readonly
+    this["2fa"] = userData.attributes["2fa"];
+    this.created_at = new Date(userData.attributes.created_at);
+    this.updated_at = new Date(userData.attributes.updated_at);
+    relationships = userData.attributes.relationships;
+  }
+
+  /**
+   * Delete this user from the panel
+   */
+  public async delete(): Promise<void> {
+    const endpoint = new URL(
+      client.panel + "/api/application/users/" + this.id
+    );
+    await client.api({ url: endpoint.href, method: "DELETE" });
+  }
+
+  private updateProps() {
+    return {
+      email: this.email,
+      username: this.username,
+      first_name: this.first_name,
+      last_name: this.last_name,
+      language: this.language,
+      root_admin: this.root_admin ? 1 : 0,
+      password: "",
+    };
+  }
+
+  private updateThisUser(user: RawPanelUser) {
+    this.email = user.attributes.email;
+    this.username = user.attributes.username;
+    this.first_name = user.attributes.first_name;
+    this.last_name = user.attributes.last_name;
+    this.language = user.attributes.language;
+    this.root_admin = user.attributes.root_admin;
+    this.updated_at = new Date(user.attributes.updated_at);
+  }
+
+  /**
+   * Update this user's email address on the panel
+   * @param email The new email address
+   */
+  public async setEmail(email: string): Promise<void> {
+    var data = this.updateProps();
+    data.email = email;
+    const endpoint = new URL(
+      client.panel + "/api/application/users/" + this.id
+    );
+    this.updateThisUser(
+      (await client.api({
+        url: endpoint.href,
+        method: "PATCH",
+        data: data,
+      })) as RawPanelUser
+    );
+  }
+
+  /**
+   * Update this user's username on the panel
+   * @param username The new username
+   */
+  public async setUsername(username: string): Promise<void> {
+    var data = this.updateProps();
+    data.username = username;
+    const endpoint = new URL(
+      client.panel + "/api/application/users/" + this.id
+    );
+    this.updateThisUser(
+      (await client.api({
+        url: endpoint.href,
+        method: "PATCH",
+        data: data,
+      })) as RawPanelUser
+    );
+  }
+
+  /**
+   * Update this user's first name on the panel
+   * @param firstName The new first name
+   */
+  public async setFirstName(firstName: string): Promise<void> {
+    var data = this.updateProps();
+    data.first_name = firstName;
+    const endpoint = new URL(
+      client.panel + "/api/application/users/" + this.id
+    );
+    this.updateThisUser(
+      (await client.api({
+        url: endpoint.href,
+        method: "PATCH",
+        data: data,
+      })) as RawPanelUser
+    );
+  }
+
+  /**
+   * Update this user's last name on the panel
+   * @param lastName The new last name
+   */
+  public async setLastName(lastName: string): Promise<void> {
+    var data = this.updateProps();
+    data.last_name = lastName;
+    const endpoint = new URL(
+      client.panel + "/api/application/users/" + this.id
+    );
+    this.updateThisUser(
+      (await client.api({
+        url: endpoint.href,
+        method: "PATCH",
+        data: data,
+      })) as RawPanelUser
+    );
+  }
+
+  /**
+   * Update this user's language on the panel
+   * @param language The new language
+   * @default language en
+   */
+  public async setLanguage(language: string): Promise<void> {
+    var data = this.updateProps();
+    data.language = language;
+    const endpoint = new URL(
+      client.panel + "/api/application/users/" + this.id
+    );
+    this.updateThisUser(
+      (await client.api({
+        url: endpoint.href,
+        method: "PATCH",
+        data: data,
+      })) as RawPanelUser
+    );
+  }
+
+  /**
+   * Update this user's password on the panel
+   * @param password The new password
+   */
+  public async setPassword(password: string): Promise<void> {
+    var data = this.updateProps();
+    data.password = password;
+    const endpoint = new URL(
+      client.panel + "/api/application/users/" + this.id
+    );
+    this.updateThisUser(
+      (await client.api({
+        url: endpoint.href,
+        method: "PATCH",
+        data: data,
+      })) as RawPanelUser
+    );
+  }
+
+  /**
+   * Update this user's admin status on the panel
+   * @param panelAdmin Should the user be an admin
+   * @default panelAdmin false
+   */
+  public async setPanelAdmin(panelAdmin: boolean): Promise<void> {
+    var data = this.updateProps();
+    data.root_admin = panelAdmin ? 1 : 0;
+    const endpoint = new URL(
+      client.panel + "/api/application/users/" + this.id
+    );
+    this.updateThisUser(
+      (await client.api({
+        url: endpoint.href,
+        method: "PATCH",
+        data: data,
+      })) as RawPanelUser
+    );
+  }
+}
