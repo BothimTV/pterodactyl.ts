0a83beea9a4cd21b98b043c2de0636fd4cb9ad5b
BothimTV <development@bothimtv.com>
2024-06-16 21:36:11 +0200

    Update the modify options for a server (properties & build configuration)

--- src/ApplicationClient/Server.ts
+++ src/ApplicationClient/Server.ts
@@ -1,4 +1,4 @@
-import { ServerUpdateBuildProperties, ServerUpdateProperties, ServerUpdateStartupProperties } from "../../types/RequestBodies";
+import { ServerUpdateStartupProperties } from "../../types/RequestBodies";
 import { ServerDatabaseBuilder } from "../builder/ServerDatabaseBuilder";
 import { RawServerDatabase, RawServerDatabaseList } from "../types/database";
 import { RawLocation } from "../types/location";
@@ -12,7 +12,9 @@ import { RawServerSubUserList } from "../types/serverSubUser";
 import { RawServerVariableList } from "../types/serverVariable";
 import { RawUser } from "../types/user";
 import { ApplicationClient } from "./ApplicationClient";
+import { NodeAllocation } from "./NodeAllocation";
 import { ServerDatabase } from "./ServerDatabase";
+import { User } from "./User";
 
 var client: ApplicationClient
 export class Server implements ServerAttributes {
@@ -86,33 +88,234 @@ export class Server implements ServerAttributes {
         this.created_at = new Date(serverProperties.attributes.created_at);
     }
 
+    private updateProps() {
+        return {
+            name: this.name,
+            external_id: this.external_id,
+            owner_id: this.user,
+            description: this.description
+        }
+    }
+
+    private updateThisProps(server: RawServer) {
+        this.name = server.attributes.name;
+        this.user = server.attributes.user;
+        this.external_id = server.attributes.external_id;
+        this.description = server.attributes.description;
+        this.updated_at = new Date(server.attributes.updated_at);
+    }
+
     /**
-    * Update this server
-    * FIXME: @deprecated
-    */
-    public async update(serverProperties: ServerUpdateProperties): Promise<void> {
+     * Set the name for this server
+     * @param name The new name for this server
+     */
+    public async setName(name: string): Promise<void> {
+        var data = this.updateProps()
+        data.name = name;
         const endpoint = new URL(client.panel + "/api/application/servers/" + this.id + "/details");
-        const data = await client.api({ url: endpoint.href, method: "PATCH", data: serverProperties }) as RawServer
-        this.name = data.attributes.name;
-        this.user = data.attributes.user;
-        this.external_id = data.attributes.external_id;
-        this.description = data.attributes.description;
-        this.updated_at = new Date(data.attributes.updated_at);
+        this.updateThisProps(await client.api({ url: endpoint.href, method: "PATCH", data: data }) as RawServer)
     }
 
     /**
-    * Update this server build details
-    * FIXME: @deprecated
-    */
-    public async updateBuild(serverProperties: ServerUpdateBuildProperties): Promise<void> {
-        const endpoint = new URL(client.panel + "/api/application/servers/" + this.id + "/build");
-        const data = await client.api({ url: endpoint.href, method: "PATCH", data: serverProperties }) as RawServer
-        this.allocation = data.attributes.allocation
-        this.limits = data.attributes.limits
-        this.feature_limits = data.attributes.feature_limits
-        this.updated_at = new Date(data.attributes.updated_at);
+     * Set the external id for this server
+     * @param externalId The new external id for this server
+     */
+    public async setExternalId(externalId: string): Promise<void> {
+        var data = this.updateProps()
+        data.external_id = externalId;
+        const endpoint = new URL(client.panel + "/api/application/servers/" + this.id + "/details");
+        this.updateThisProps(await client.api({ url: endpoint.href, method: "PATCH", data: data }) as RawServer)
     }
 
+    /**
+     * Set the owner for this server
+     * @param owner The ownerId or the User object
+     */
+    public async setOwner(owner: number | User): Promise<void> {
+        var data = this.updateProps()
+        data.owner_id = typeof owner === "number" ? owner : owner.id;
+        const endpoint = new URL(client.panel + "/api/application/servers/" + this.id + "/details");
+        this.updateThisProps(await client.api({ url: endpoint.href, method: "PATCH", data: data }) as RawServer)
+    }
+
+    /**
+     * Set the description for this server
+     * @param description The new description for this server
+     */
+    public async setDescription(description: string): Promise<void> {
+        var data = this.updateProps()
+        data.description = description;
+        const endpoint = new URL(client.panel + "/api/application/servers/" + this.id + "/details");
+        this.updateThisProps(await client.api({ url: endpoint.href, method: "PATCH", data: data }) as RawServer)
+    }
+
+    private updateBuild() {
+        return {
+            cpu: this.limits.cpu,
+            threads: this.limits.threads,
+            memory: this.limits.memory,
+            swap: this.limits.swap,
+            disk: this.limits.disk,
+            io: this.limits.io,
+            oom_disabled: this.limits.oom_disabled ? 1 : 0,
+            database_limit: this.feature_limits.databases,
+            allocation_limit: this.feature_limits.allocations,
+            backup_limit: this.feature_limits.backups,
+            allocation_id: this.allocation,
+        }
+    }
+
+    private updateThisBuild(server: RawServer) {
+        this.name = server.attributes.name;
+        this.user = server.attributes.user;
+        this.external_id = server.attributes.external_id;
+        this.description = server.attributes.description;
+        this.updated_at = new Date(server.attributes.updated_at);
+    }
+
+    /**
+     * Set the maximum cpu usage for this server  
+     * Set this to 0 for no cpu limit  
+     * ------------------------  
+     * If you have 4 cores and want to allow to use all of them you'll need to use 400  
+     * Example: 4 * 100 = 400 | Cores * 100 = Max for this node  
+     * ------------------------  
+     * @param limit The new cpu limit in %
+     */
+    public async setCpuLimit(limit: number): Promise<void> {
+        var data = this.updateBuild()
+        data.cpu = limit;
+        const endpoint = new URL(client.panel + "/api/application/servers/" + this.id + "/build");
+        this.updateThisBuild(await client.api({ url: endpoint.href, method: "PATCH", data: data }) as RawServer)
+    }
+
+    /**
+     * Set which cores can be used by a server  
+     * Example: 0; 1-3; 4,5,6;
+     * @param limit The new cpu limit in %
+     */
+    public async setCpuPinning(pinning: Array<string> | string): Promise<void> {
+        var data = this.updateBuild()
+        data.threads = typeof pinning === "string" ? pinning : pinning.join(",");
+        const endpoint = new URL(client.panel + "/api/application/servers/" + this.id + "/build");
+        this.updateThisBuild(await client.api({ url: endpoint.href, method: "PATCH", data: data }) as RawServer)
+    }
+
+    /**
+     * Set the new memory limit
+     * @param limit The new memory limit in MiB
+     */
+    public async setMemoryLimit(limit: number): Promise<void> {
+        var data = this.updateBuild()
+        data.memory = limit;
+        const endpoint = new URL(client.panel + "/api/application/servers/" + this.id + "/build");
+        this.updateThisBuild(await client.api({ url: endpoint.href, method: "PATCH", data: data }) as RawServer)
+    }
+
+    /**
+     * Set the new swap limit
+     * @param limit The new swap limit in MiB
+     */
+    public async setSwapLimit(limit: number): Promise<void> {
+        var data = this.updateBuild()
+        data.swap = limit;
+        const endpoint = new URL(client.panel + "/api/application/servers/" + this.id + "/build");
+        this.updateThisBuild(await client.api({ url: endpoint.href, method: "PATCH", data: data }) as RawServer)
+    }
+
+    /**
+     * Set the new disk limit
+     * @param limit The new disk limit in MiB
+     */
+    public async setDiskLimit(limit: number): Promise<void> {
+        var data = this.updateBuild()
+        data.disk = limit;
+        const endpoint = new URL(client.panel + "/api/application/servers/" + this.id + "/build");
+        this.updateThisBuild(await client.api({ url: endpoint.href, method: "PATCH", data: data }) as RawServer)
+    }
+
+    /**
+     * [ADVANCED]  
+     * Set the io limit for this server  
+     * Documentation: https://docs.docker.com/engine/reference/run/#block-io-bandwidth-blkio-constraint  
+     * @param limit The new io limit, number between 10 and 1000
+     */
+    public async setIoLimit(limit: number): Promise<void> {
+        var data = this.updateBuild()
+        data.io = limit;
+        const endpoint = new URL(client.panel + "/api/application/servers/" + this.id + "/build");
+        this.updateThisBuild(await client.api({ url: endpoint.href, method: "PATCH", data: data }) as RawServer)
+    }
+
+    /**
+     * Enable the OOM killer  
+     * This will kill the server if it exceeds the memory limit  
+     * This may cause the server processes to exit unexpectedly  
+     * This CAN cause to data corruption
+     * @param active Should the oom killer be active
+     */
+    public async setOomKillerState(active: boolean): Promise<void> {
+        var data = this.updateBuild()
+        data.oom_disabled = active ? 0 : 1;
+        const endpoint = new URL(client.panel + "/api/application/servers/" + this.id + "/build");
+        this.updateThisBuild(await client.api({ url: endpoint.href, method: "PATCH", data: data }) as RawServer)
+    }
+
+    /**
+     * Set the new database limit
+     * @param limit How many databases can be created for this server
+     */
+    public async setDatabaseLimit(limit: number): Promise<void> {
+        var data = this.updateBuild()
+        data.database_limit = limit;
+        const endpoint = new URL(client.panel + "/api/application/servers/" + this.id + "/build");
+        this.updateThisBuild(await client.api({ url: endpoint.href, method: "PATCH", data: data }) as RawServer)
+    }
+
+    /**
+     * Set the new backup limit
+     * @param limit How many backups can be created for this server
+     */
+    public async setBackupLimit(limit: number): Promise<void> {
+        var data = this.updateBuild()
+        data.backup_limit = limit;
+        const endpoint = new URL(client.panel + "/api/application/servers/" + this.id + "/build");
+        this.updateThisBuild(await client.api({ url: endpoint.href, method: "PATCH", data: data }) as RawServer)
+    }
+
+    /**
+     * Set the new allocation limit
+     * @param limit How many allocations can be created for this server
+     */
+    public async setAllocationLimit(limit: number): Promise<void> {
+        var data = this.updateBuild()
+        data.allocation_limit = limit;
+        const endpoint = new URL(client.panel + "/api/application/servers/" + this.id + "/build");
+        this.updateThisBuild(await client.api({ url: endpoint.href, method: "PATCH", data: data }) as RawServer)
+    }
+
+    /**
+     * Add a allocation to this server
+     * @param allocation The allocation id or the NodeAllocation object
+     */
+    public async addAllocation(allocation: number | NodeAllocation): Promise<void> {
+        var data = this.updateBuild() as any
+        data.add_allocations[typeof allocation == "number" ? allocation : allocation.id] = typeof allocation == "number" ? allocation : allocation.id
+        const endpoint = new URL(client.panel + "/api/application/servers/" + this.id + "/build");
+        this.updateThisBuild(await client.api({ url: endpoint.href, method: "PATCH", data: data }) as RawServer)
+    }
+
+    /**
+     * Remove a allocation from this server
+     * @param allocation The allocation id or the NodeAllocation object
+     */
+    public async removeAllocation(allocation: number | NodeAllocation): Promise<void> {
+        var data = this.updateBuild() as any
+        data.remove_allocations[typeof allocation == "number" ? allocation : allocation.id] = typeof allocation == "number" ? allocation : allocation.id
+        const endpoint = new URL(client.panel + "/api/application/servers/" + this.id + "/build");
+        this.updateThisBuild(await client.api({ url: endpoint.href, method: "PATCH", data: data }) as RawServer)
+    }
+    
     /**
      * Update this server startup details
      * FIXME: @deprecated
