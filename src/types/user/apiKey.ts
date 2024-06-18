fbe187b853e1ccdca8ce95f37f8cc92d8f7d15a1
Merlin <development@bothimtv.com>
2024-06-18 11:40:57 +0200

    Formatting :)

--- src/types/user/apiKey.ts
+++ src/types/user/apiKey.ts
@@ -1,17 +1,17 @@
 export interface RawApiKeyList {
-    object: "list";
-    data: Array<RawApiKey>;
+  object: "list";
+  data: Array<RawApiKey>;
 }
 
 export interface RawApiKey {
-    object: "api_key";
-    attributes: ApiKeyAttributes;
+  object: "api_key";
+  attributes: ApiKeyAttributes;
 }
 
-export interface ApiKeyAttributes  {
-    "identifier": string
-    "description": string,
-    "allowed_ips": Array<string>,
-    "last_used_at": string | Date,
-    "created_at": string | Date
+export interface ApiKeyAttributes {
+  identifier: string;
+  description: string;
+  allowed_ips: Array<string>;
+  last_used_at: string | Date;
+  created_at: string | Date;
 }
