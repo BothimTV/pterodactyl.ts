64af46bc92a3e6b8468320666f8160cd9405fc90
BothimTV <development@bothimtv.com>
2024-06-22 17:31:01 +0200

    Fixed copy-paste typo

--- src/github-util/npm_publish.js
+++ src/github-util/npm_publish.js
@@ -1,7 +1,7 @@
 const { readFileSync, writeFileSync } = require('fs')
 
-const data = JSON.parse(readFileSync('../package.json', 'utf8'))
+const data = JSON.parse(readFileSync('./package.json', 'utf8'))
 
 data.name = "pterodactyl.ts"
 
-writeFileSync("../package.json", JSON.stringify(data, null, 2), "utf8")
+writeFileSync("./package.json", JSON.stringify(data, null, 2), "utf8")
