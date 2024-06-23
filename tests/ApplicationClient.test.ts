26d9f6795d65940f833b4af40d51c8d8c84adac1
BothimTV <development@bothimtv.com>
2024-06-23 15:53:15 +0200

    Initial implementation of testcontainers for integration tests

--- tests/ApplicationClient.test.ts
+++ tests/ApplicationClient.test.ts
@@ -1,195 +1,67 @@
 import { afterAll, beforeAll, describe, expect, test } from "@jest/globals";
-import {
-  GenericContainer,
-  Network,
-  StartedNetwork,
-  StartedTestContainer
-} from "testcontainers";
-import { ApplicationClient } from "../src/ApplicationClient/ApplicationClient";
-import { AllocationBuilder } from "../src/builder/AllocationBuilder";
-import { NodeBuilder } from "../src/builder/NodeBuilder";
-import { UserBuilder } from "../src/builder/UserBuilder";
+import { StartedTestContainer } from "testcontainers";
+import { ApplicationClient, LocationBuilder, NodeBuilder, PanelLocation, PanelNode, UserClient } from "../dist";
+import { cleanup, getNetwork, getPanel, startWings } from "./containerProvider";
 
-const client = new ApplicationClient({
-  apikey: process.env.API_KEY || "",
-  panel: process.env.PANEL || ""
+let applicationClient: ApplicationClient
+let userClient: UserClient
+let location: PanelLocation
+let node: PanelNode
+
+let panel: StartedTestContainer
+let wings: StartedTestContainer
+
+beforeAll(async () => {
+  panel = await getPanel()
+  console.log("Received panel")
+  applicationClient = new ApplicationClient({
+    panel: "http://localhost:" + panel.getMappedPort(80),
+    apikey: "ptla_CiTiqLaesSlv6MwQq0ZgvmQAeopqTFBfgO7vKXuBIAv"
+  })
+  userClient = new UserClient({
+    panel: "http://localhost:" + panel.getMappedPort(80),
+    apikey: "ptlc_A06HxoSpOGMuEa7lYCZPezVXfWogd2YSn2CiEZwS2Id"
+  })
+
+  location = await applicationClient.createLocation(
+    new LocationBuilder()
+      .setShort("test")
+      .setDescription("Test Location")
+  )
+  console.log("Created location: " + location.id)
+
+  node = await applicationClient.createNode(
+    new NodeBuilder()
+      .setName("test")
+      .setDescription("Test Node")
+      .setLocationId(location.id)
+      .setPublic(true)
+      .setFqdn(panel.getIpAddress(getNetwork().getName()).split(".").map((v, i) => i === 3 ? 5 : v).join("."))
+      .setScheme("http")
+      .setDisk(1000)
+      .setMemory(1000)
+  )
+  console.log("Created node: " + node.id)
+  wings = await startWings(await node.getConfiguration())
+
+}, 2 * 60 * 1000)
+
+describe("Test the ApplicationClient", () => {
+  test("Test initial data", async () => {
+
+    const nodes = await applicationClient.getNodes()
+    const users = await applicationClient.getUsers()
+    const locations = await applicationClient.getLocations()
+
+    expect(nodes.length).toBe(1)
+    expect(users.length).toBe(1)
+    expect(locations.length).toBe(1)
+
+  })
+
+
 });
 
-describe("Test the Application API", () => {
-  var testUserId: number
-  var testNodeId: number
-  let panel: StartedTestContainer
-  let wings: StartedTestContainer
-  let network: StartedNetwork
-  beforeAll(async () => {
-    network = await new Network().start();
-    panel = await new GenericContainer("ghcr.io/pterodactyl/panel")
-      .withNetwork(network)
-      .start();
-    wings = await new GenericContainer("ghcr.io/pterodactyl/wings")
-      .withNetwork(network)
-      .start();
-    testUserId = await client.createUser(
-      new UserBuilder()
-        .setEmail("test@test.de")
-        .setUsername("test")
-        .setFirstName("test")
-        .setLastName("test")
-        .setPassword("test")
-    ).then((user) => {
-      return user.id
-    }).catch((e) => { return 200 })
-    testNodeId = await client.createNode(
-      new NodeBuilder()
-        .setName("Test Node")
-        .setLocationId(1)
-        .setFqdn("test.de")
-        .setMemory(1024)
-        .setDisk(1024)
-    ).then(async (node) => {
-      await node.createAllocation(
-        new AllocationBuilder()
-          .setIp("0.0.0.0")
-          .setAlias("test-node.test.de")
-          .addPort(25565)
-          .addPorts([25566, 25567])
-      )
-      return node.id
-    })
-  })
-  test("Try getting & managing users", async () => {
-    // file deepcode ignore PromiseNotCaughtGeneral/test: These are the test, its "good" if they can fail
-
-    await client.getUser(testUserId).then(async (user) => {
-      console.log(user)
-      const oldUsrData = await client.getUser(testUserId);
-      await user.setEmail("test2@test.de").then(() => { })
-      expect(user.email).toBe("test2@test.de");
-      expect(oldUsrData.email).not.toBe("test2@test.de");
-
-      await user.setUsername("test2")
-      expect(user.username).toBe("test2");
-      expect(oldUsrData.username).not.toBe("test2");
-
-      await user.setFirstName("test2")
-      expect(user.first_name).toBe("test2");
-      expect(oldUsrData.first_name).not.toBe("test2");
-
-      await user.setLastName("test2")
-      expect(user.last_name).toBe("test2");
-      expect(oldUsrData.last_name).not.toBe("test2");
-
-      // NOT TESTABLE - There is no other language than en installed by default
-      //await user.setLanguage("de")
-      //expect(user.language).toBe("de");
-      //expect(oldUsrData.language).not.toBe("de")
-
-      await user.setPassword("test2")
-
-      await user.setPanelAdmin(true)
-      expect(user.root_admin).toBe(true);
-      expect(oldUsrData.root_admin).toBe(false);
-      console.log(user)
-
-    });
-  })
-
-  test("Try getting & managing nodes", async () => {
-    await client.getNode(testNodeId).then(async (node) => {
-      console.log(node)
-
-      const allocations = await node.getAllocations()
-      console.log(allocations)
-
-      const oldNodeData = await client.getNode(testNodeId)
-      await node.setName("test2")
-      node = await client.getNode(testNodeId)
-      expect(node.name).toBe("test2")
-      expect(oldNodeData.name).not.toBe("test2")
-
-      await node.setDescription("test2")
-      node = await client.getNode(testNodeId)
-      expect(node.description).toBe("test2")
-      expect(oldNodeData.description).not.toBe("test2")
-
-      // This is currently untestable as we don't create 2 locations
-      // await node.setLocation(1)
-      // expect(node.location_id).toBe(1)
-      // expect(oldNodeData.location_id).not.toBe(1)
-
-      await node.setPublic(false)
-      node = await client.getNode(testNodeId)
-      expect(node.public).toBe(false)
-      expect(oldNodeData.public).toBe(true)
-
-      await node.setFqdn("test2.de")
-      node = await client.getNode(testNodeId)
-      expect(node.fqdn).toBe("test2.de")
-      expect(oldNodeData.fqdn).not.toBe("test2.de")
-
-      await node.setScheme("http")
-      node = await client.getNode(testNodeId)
-      expect(node.scheme).toBe("http")
-      expect(oldNodeData.scheme).not.toBe("http")
-
-      await node.setBehindProxy(true)
-      node = await client.getNode(testNodeId)
-      expect(node.behind_proxy).toBe(true)
-      expect(oldNodeData.behind_proxy).toBe(false)
-
-      await node.setMaintenance(true)
-      node = await client.getNode(testNodeId)
-      expect(node.maintenance_mode).toBe(true)
-      expect(oldNodeData.maintenance_mode).toBe(false)
-
-      // Disable the maintenance mode to be able to use this node for later tests
-      await node.setMaintenance(false)
-
-      await node.setMemory(2048)
-      node = await client.getNode(testNodeId)
-      expect(node.memory).toBe(2048)
-      expect(oldNodeData.memory).not.toBe(2048)
-
-      await node.setMemoryOverAllocation(-1)
-      node = await client.getNode(testNodeId)
-      expect(node.memory_overallocate).toBe(-1)
-      expect(oldNodeData.memory_overallocate).not.toBe(-1)
-
-      await node.setDisk(2048)
-      node = await client.getNode(testNodeId)
-      expect(node.disk).toBe(2048)
-      expect(oldNodeData.disk).not.toBe(2048)
-
-      await node.setDiskOverAllocation(-1)
-      node = await client.getNode(testNodeId)
-      expect(node.disk_overallocate).toBe(-1)
-      expect(oldNodeData.disk_overallocate).not.toBe(-1)
-
-      await node.setUploadSizeLimit(200)
-      node = await client.getNode(testNodeId)
-      expect(node.upload_size).toBe(200)
-      expect(oldNodeData.upload_size).not.toBe(200)
-
-      await node.setDaemonPort(8443)
-      node = await client.getNode(testNodeId)
-      expect(node.daemon_listen).toBe(8443)
-      expect(oldNodeData.daemon_listen).not.toBe(8443)
-
-      await node.setDaemonSftpPort(2023)
-      node = await client.getNode(testNodeId)
-      expect(node.daemon_sftp).toBe(2023)
-      expect(oldNodeData.daemon_sftp).not.toBe(2023)
-
-      await node.resetDaemonMasterKey()
-
-      console.log(node)
-    })
-  }, 60000)
-
-  afterAll(async () => {
-    await (await client.getUser(testUserId)).delete()
-    //await (await client.getNode(testNodeId)).delete()
-    await panel.stop()
-    await wings.stop()
-  })
-});
+afterAll(async () => {
+  await cleanup()
+}, 2 * 60 * 1000)
---
+++ tests/containerProvider.ts
@@ -0,0 +1,122 @@
+import { readFileSync } from "fs";
+import { createPool } from "mariadb";
+import {
+    GenericContainer,
+    Network,
+    StartedNetwork,
+    StartedTestContainer
+} from "testcontainers";
+import { RawNodeConfiguration } from "../src/types/application/nodeConfiguration";
+
+let redis: StartedTestContainer;
+let mysql: StartedTestContainer;
+let panel: StartedTestContainer;
+let wings: StartedTestContainer;
+let network: StartedNetwork;
+
+async function startContainers() {
+    network = await new Network()
+        .start();
+    console.log("Network is running")
+
+    redis = await new GenericContainer("redis:latest")
+        .withNetwork(network)
+        .start();
+    console.log("Redis is running: ", redis.getIpAddress(network.getName()))
+
+    mysql = await new GenericContainer("mariadb:latest")
+        .withNetwork(network)
+        .withEnvironment({
+            "MARIADB_USER": "pterodactyl",
+            "MARIADB_PASSWORD": "password",
+            "MARIADB_DATABASE": "panel",
+            "MARIADB_ALLOW_EMPTY_ROOT_PASSWORD": "1"
+        })
+        .withExposedPorts(3306)
+        .start();
+
+    await mysql.copyContentToContainer([{
+        content: readFileSync("./tests/devSetup.sql", { encoding: "utf-8" }),
+        target: "/docker-entrypoint-initdb.d/devSetup.sql",
+    }])
+    console.log("Mysql is running: " + mysql.getIpAddress(network.getName()) + ":3006")
+    console.log("Mysql is running: localhost:", mysql.getMappedPort(3306))
+
+    let panelUrl = "http://" + mysql.getIpAddress(network.getName()).split(".").map((v, i) => i === 3 ? 4 : v).join(".")
+    panel = await new GenericContainer("ghcr.io/pterodactyl/panel")
+        .withNetwork(network)
+        .withEnvironment({
+            "APP_URL": panelUrl,
+            "APP_DEBUG": "true",
+            "APP_TIMEZONE": "Europe/Berlin",
+            "DB_HOST": mysql.getIpAddress(network.getName()),
+            "DB_PORT": "3306",
+            "DB_DATABASE": "panel",
+            "DB_USERNAME": "pterodactyl",
+            "DB_PASSWORD": "password",
+            "CACHE_DRIVER": "redis",
+            "REDIS_HOST": redis.getIpAddress(network.getName()),
+            "REDIS_PORT": "6379",
+            "REDIS_PASSWORD": "",
+            "APP_KEY": "qRR876pqQ5pbn3zxC28aZ3TZjTwczXPp"
+        })
+        .withExposedPorts(80)
+        .start();
+
+    await new Promise(async (resolve) => {
+        (await panel.logs()).on("data", (data: string) => {
+            if (data.includes("Starting cron jobs.")) resolve("")
+        })
+    })
+
+    await executeMysql(mysql.getMappedPort(3306))
+
+    console.log("Panel is running", panel.getIpAddress(network.getName()))
+    console.log("Panel url: http://localhost:" + panel.getMappedPort(80))
+}
+
+async function executeMysql(port: number) {
+    const pool = createPool({
+        host: "localhost",
+        port: port,
+        user: "pterodactyl",
+        password: "password",
+        database: "panel",
+    })
+    const queries = readFileSync("./tests/devSetup.sql", { encoding: "utf-8" }).split("\n")
+    for (const query of queries) {
+        await pool.execute(query).catch((err) => console.log(err))
+    }
+    await pool.end().catch((err) => console.log(err))
+}
+
+export function getNetwork(): StartedNetwork {
+    return network;
+}
+
+
+export async function startWings(cfg: RawNodeConfiguration): Promise<StartedTestContainer> {
+    console.log("Starting wings")
+    wings = await new GenericContainer("ghcr.io/pterodactyl/wings")
+        .withNetwork(network)
+        .withCopyContentToContainer([{
+            content: JSON.stringify(cfg),
+            target: "/etc/pterodactyl/config.yml",
+        }])
+        .start();
+    console.log("Wings is running", wings.getIpAddress(network.getName()))
+    return wings;
+}
+
+export async function getPanel(): Promise<StartedTestContainer> {
+    await startContainers();
+    return panel;
+}
+
+export async function cleanup(): Promise<void> {
+    await panel.stop();
+    await wings.stop();
+    await mysql.stop();
+    await redis.stop()
+    await network.stop();
+}
