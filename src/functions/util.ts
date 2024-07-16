7f8c595283b42afee5cef643ce88d1313c8d43b6
BothimTV <development@bothimtv.com>
2024-07-16 17:16:41 +0200

    Added string option to smartConvert

--- src/functions/util.ts
+++ src/functions/util.ts
@@ -4,6 +4,7 @@ export function smartConvert(
 ): {
   value: number;
   unit: string;
+  string: string;
 } {
   if (bytesToGigabytes(bytes, 0) == 0) {
     // > 1GB
@@ -11,13 +12,13 @@ export function smartConvert(
       // > 1MB
       if (bytesToKilobytes(bytes, 0) == 0) {
         // > 1KB
-        return { value: bytes, unit: "Bytes" };
+        return { value: bytes, unit: "Bytes", string: `${bytes} Bytes` };
       }
-      return { value: bytesToKilobytes(bytes, comma), unit: "KiB" };
+      return { value: bytesToKilobytes(bytes, comma), unit: "KiB", string: `${bytesToKilobytes(bytes, comma)} KiB`  };
     }
-    return { value: bytesToMegabytes(bytes, comma), unit: "MiB" };
+    return { value: bytesToMegabytes(bytes, comma), unit: "MiB", string: `${bytesToMegabytes(bytes, comma)} MiB`  };
   }
-  return { value: bytesToGigabytes(bytes, comma), unit: "GiB" };
+  return { value: bytesToGigabytes(bytes, comma), unit: "GiB", string: `${bytesToGigabytes(bytes, comma)} GiB`  };
 }
 
 function bytesToGigabytes(bytes: number, comma: number = 3): number {
